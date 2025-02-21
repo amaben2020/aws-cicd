f916b4e94b36c990fc4aa20b4289f69f7223ebad
amaben2020 <60056927+amaben2020@users.noreply.github.com>
2025-02-21 10:57:54 +0100

    update

--- lib/aws-codepipeline-stack.ts
+++ lib/aws-codepipeline-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import {
+  CodeBuildStep,
   CodePipeline,
   CodePipelineSource,
   ShellStep,
@@ -23,6 +24,12 @@ export class AwsCodepipelineStack extends cdk.Stack {
     const testStage = pipeline.addStage(
       new PipelineStage(this, 'PipelineTestStage', {
         stageName: 'test',
+      })
+    );
+
+    testStage.addPre(
+      new CodeBuildStep('unit-tests', {
+        commands: ['cd aws-codepipeline', 'npm ci', 'npx test'],
       })
     );
   }
